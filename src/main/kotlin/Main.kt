4fe0162e67c777e4bc0227043826e8672f2f3c38
AmakoveBridgit <amakovebridgit@gmail.com>
2023-02-03 11:48:09 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    fun main(){
+        var name="Eliza"
+        var age="19"
+        var weight="55kgs"
+        var citizenship="Ugandan"
+        var phonenumber="0716588562"
+        var citizenshipUgandan=true
+
+
+
+        println(name)
+        println(age)
+        println(weight)
+        println(citizenship)
+        println(phonenumber)
+        println("Ugandan")
+
+
+
+    }
+
+}
